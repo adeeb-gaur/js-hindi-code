4cbe898a0cc3a1808dba949c10d9a2f9ae0fe8fe
adeeb-gaur <adeebgourgour@gmail.com>
2026-09-10 08:54:31 +0000

    my_first_code

--- adeeb.js
+++ adeeb.js
@@ -1,1 +1,7 @@
 console.log("Hello, world.");
+
+
+var a = 10 
+var b = 20 
+var c = a + b
+console.log(c);
---
+++ code.js
@@ -0,0 +1,7 @@
+console.log("Helloworld");
+
+var a = 10 
+var b = 20 
+var c = a + b
+console.log(c);
+
